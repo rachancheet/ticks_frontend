515828aee5ae42db6665553efcff843af371ad4c
rachancheet <raxxsngh37@gmail.com>
2024-01-21 03:31:43 +0530

    added_title

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <title>Tic Tac Toe</title>
       <body className={inter.className}>{children}</body>
     </html>
   );
